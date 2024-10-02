c8fecb0a6ca28ec7ada2e8f163361757fb6c9a17
MtkN1 <51289448+MtkN1@users.noreply.github.com>
2024-10-03 01:03:35 +0900

    Add apt.d

--- src/installer/apt.sh
+++ src/installer/apt.sh
@@ -21,3 +21,7 @@ sudo apt-get --no-install-recommends -y build-dep "${source_packages[@]}"
 
 sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
+
+for script in $(dirname "${0}")/apt.d/*.sh; do
+    "${script}"
+done
