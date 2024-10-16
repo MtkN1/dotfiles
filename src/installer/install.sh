6ddc70d59822033aee51acd1e50666aa9f9939e9
MtkN1 <51289448+MtkN1@users.noreply.github.com>
2024-10-16 09:07:40 +0900

    Synchronise install and upgrades scripts

--- src/installer/install.sh
+++ src/installer/install.sh
@@ -2,5 +2,6 @@
 
 "$(dirname "${0}")/apt.sh"
 "$(dirname "${0}")/binary.sh"
-"$(dirname "${0}")/source.sh"
 "$(dirname "${0}")/uv_tool.sh"
+"$(dirname "${0}")/source.sh"
+"$(dirname "${0}")/image.sh"
--- src/installer/upgrade.sh
+++ src/installer/upgrade.sh
@@ -2,7 +2,9 @@
 
 sudo apt-get update
 sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
 
+"$(dirname "${0}")/binary.sh"
 "$(dirname "${0}")/uv_tool.sh"
 "$(dirname "${0}")/source.sh"
 "$(dirname "${0}")/image.sh"
