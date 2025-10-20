6494688dd648d9991c3ee46812e31e8aff2375bf
MtkN1 <51289448+MtkN1@users.noreply.github.com>
2025-10-20 16:23:20 +0900

    Update to trixie

--- src/installer/image.sh
+++ src/installer/image.sh
@@ -3,14 +3,14 @@
 images=(
     # base images
     mcr.microsoft.com/devcontainers/base:alpine
-    mcr.microsoft.com/devcontainers/base:bookworm
+    mcr.microsoft.com/devcontainers/base:trixie
     mcr.microsoft.com/devcontainers/base:noble
     mcr.microsoft.com/devcontainers/universal:2-linux
 
     # language images
-    python:3.13-slim-bookworm
-    python:3.13-bookworm
-    mcr.microsoft.com/devcontainers/python:3.13-bookworm
+    python:3.13-slim-trixie
+    python:3.13-trixie
+    mcr.microsoft.com/devcontainers/python:3.13-trixie
 )
 
 for image in "${images[@]}"; do
