279591b109020ac6a43c966a791eddaaa38ce245
MtkN1 <51289448+MtkN1@users.noreply.github.com>
2025-10-22 17:27:16 +0900

    Rename uv and uvx to command names

--- src/installer/uv_tool.sh
+++ src/installer/uv_tool.sh
@@ -7,33 +7,31 @@ packages=(
     'ruff'
 )
 
-_install_dir="${CARGO_HOME:-${HOME}/.cargo}/bin"
-_user_local_bin="${HOME}/.local/bin"
 _completions_dir="${BASH_COMPLETION_USER_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/bash-completion}/completions"
 
 # Check
-if ! command -v "${_install_dir}/uv" >/dev/null 2>&1; then
-    eixt 1
+if ! command -v uv >/dev/null 2>&1; then
+    exit 1
 fi
 
 # Install (package name == command name)
 for package in "${packages[@]}"; do
-    if ! command -v "${_user_local_bin}/${package}" >/dev/null 2>&1; then
-        "${_install_dir}/uv" tool install "${package}"
+    if ! command -v "${package}" >/dev/null 2>&1; then
+        uv tool install "${package}"
     fi
 done
 # Install (package name != command name)
-if ! command -v "${_user_local_bin}/aws" >/dev/null 2>&1; then
-    "${_install_dir}/uv" tool install awscli
+if ! command -v aws >/dev/null 2>&1; then
+    uv tool install awscli
 fi
 # Install (self-hosted)
-if ! command -v "${_user_local_bin}/gpkg" >/dev/null 2>&1; then
-    "${_install_dir}/uv" tool install --from git+https://github.com/MtkN1/gpkg.git gpkg
+if ! command -v gpkg >/dev/null 2>&1; then
+    uv tool install --from git+https://github.com/MtkN1/gpkg.git gpkg
 fi
 
 # Upgrade
-"${_install_dir}/uv" tool upgrade --all
-"${_user_local_bin}/gpkg" upgrade
+uv tool upgrade --all
+gpkg upgrade
 
 # Completions
 mkdir --parents "${_completions_dir}"
@@ -41,10 +39,10 @@ mkdir --parents "${_completions_dir}"
 ## awscli
 echo 'complete -C aws_completer aws' > "${_completions_dir}/aws"
 ## hatch
-_HATCH_COMPLETE=bash_source "${_user_local_bin}/hatch" > "${_completions_dir}/hatch"
+_HATCH_COMPLETE=bash_source hatch > "${_completions_dir}/hatch"
 ## pdm
-"${_user_local_bin}/pdm" completion bash > "${_completions_dir}/pdm"
+pdm completion bash > "${_completions_dir}/pdm"
 ## poetry
-"${_user_local_bin}/poetry" completions bash > "${_completions_dir}/poetry"
+poetry completions bash > "${_completions_dir}/poetry"
 ## ruff
-"${_user_local_bin}/ruff" generate-shell-completion bash > "${_completions_dir}/ruff"
+ruff generate-shell-completion bash > "${_completions_dir}/ruff"
